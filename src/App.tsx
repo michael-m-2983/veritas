ee2df5d916932dd6eaa8eaaa64ee374b1e579eee
michael-m-2983 <michaelmccright2983@gmail.com>
2025-11-02 18:52:55 -0600

    Remove old App.tsx file

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, Container, Stack, Text, TextInput } from "@mantine/core";
-
-export default function App() {
-  return <Stack>
-    <Text>Hello, world!</Text>
-    <Button>Click me!</Button>
-    <TextInput />
-  </Stack>
-}
